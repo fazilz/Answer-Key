c5bb9ebaacb29f2f9f1013d7d21bb22ef1fd2e73
FazilZuhaib <zuhaibfazil@gmail.com>
2014-03-27 17:31:03 -0400

    Added the first question in the third chapter

---
+++ 3.Functions/min.js
@@ -0,0 +1,16 @@
+// chapter 3 
+//  Recursive. 
+// Fazil Zuhaib
+
+(function () {
+	// body...
+	function min(firstNumber, secondNumber) {
+		if(firstNumber < secondNumber) {
+			console.log(firstNumber);
+		} else {
+			console.log(secondNumber);
+		}
+
+	}
+
+})();
